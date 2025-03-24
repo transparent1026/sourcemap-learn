79e59011643d268f24ba2d89cc2d575730126411
yinhaihan <835935386@qq.com>
2025-03-25 00:15:19 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <!-- <div>
+  <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -19,7 +19,7 @@ onMounted(() => {
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" /> -->
+  <HelloWorld msg="Vite + Vue" />
   <!-- <div>testNum</div> -->
 </template>
 
