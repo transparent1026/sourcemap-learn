2c48f023a0e928361c5e9478d9b949fb595b529a
yinhaihan <835935386@qq.com>
2025-03-25 00:25:01 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [vue()],
-})
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    emptyOutDir: true,
+    // rollupOptions: {
+    //   input: ['src/index.html']
+    // }
+  },
+});
