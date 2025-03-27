c107931c94a562b399a4fe2a4b7aa42eedd2de86
yinhaihan <835935386@qq.com>
2025-03-27 22:50:41 +0800

    feat: 打包测试

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,16 +2,21 @@ import axios from "axios";
 import sourceMap from "source-map-js";
 // 获取线上sourceMap文件
 const getSourceMap = async (url: string) => {
-  const res = await axios.get(url);
-  return res.data;
+  return await axios.get(url);
 };
 export const findCodeBySourceMap = async (stackFrame: any) => {
+  console.log(stackFrame, "stackFrame");
   // 获取文件
   const sourcemap = await getSourceMap(stackFrame.fileName + ".map");
+  console.log(sourcemap, "sourcemap");
+
   // 读取data
   const fileContent = sourcemap.data;
+  console.log(fileContent, "fileContent");
+
   // 解析map文件
   const consumer = await new sourceMap.SourceMapConsumer(fileContent);
+  console.log(consumer, "consumer");
   // 通过报错位置查找源文件名称以及报错行号
   const originalPosition = consumer.originalPositionFor({
     line: stackFrame.lineNumber,
