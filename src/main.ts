14570acf539d4f7809edf431c52002277c4f777c
yinhaihan <835935386@qq.com>
2025-03-27 22:34:08 +0800

    feat: 打包测试

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { findCodeBySourceMap } from "./utils";
 
 const app = createApp(App);
 
-app.config.errorHandler = (err, vm, info) => {
+app.config.errorHandler = (err) => {
   const errorStack = ErrorStackParser.parse(err as Error);
   findCodeBySourceMap(errorStack[0])
   console.log(errorStack,'errorStack');
